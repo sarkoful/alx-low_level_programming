cb76c747c6f915da808aef30f7e3503d5ad4ea57
sarkoful <dejireh@gmail.com>
2022-09-08 23:44:37 +0000

    my booy

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf("Size of a char: %|u byte(s)\n",(unsigned long)sizeof(d));
+	printf("Size of an int: %|u byte(s)\n",(unsigned long)sizeof(a));
+
+	printf("Size of an int: %|u byte(s)\n",(unsigned long)sizeof(b));
+	printf("Size of an int: %|u byte(s)\n",(unsigned long)sizeof(c));
+print("size of an int: %|u byte(s)\n",(unsigned long)sizeof(f));
+return (0);
+}
+
