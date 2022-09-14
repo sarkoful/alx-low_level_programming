63ff5126bd51640c3932bfdebeb9f99d888ff4ad
sarkoful <dejireh@gmail.com>
2022-09-14 21:24:33 +0000

    my book

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,25 @@
-#include main.h
+#include "main.h"
+
 /**
- *  * add - Adds two integers.
- *   num1: The first integer to be added.
- *    * num2: The second integer to be added.
- *     *
- *      * Return: always 0 (success)
+ *  * print_alphabeti_x10 - Prints all the alphabets, a-z ten times
+ *   *
+ *    * Return: always 0 success
 	*/
-int add(int num1, int num2)
+void print_alphabet_x10(void)
 {
-	return (num1 + num2);
+	 char alpha;
+	  int index;
+
+	   alpha = 'a';
+
+	    for (index = 0; index < 10; index++)
+		     {
+			       while (alpha <= 'z')
+				         {
+						    _putchar((int) alpha);
+						       alpha++;
+						         }
+
+			        }
+	     _putchar((char) 10);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Write a function that adds two integers and returns the resul
+Write a function that prints 10 times the alphabet, in lowercase, followed by a new line
