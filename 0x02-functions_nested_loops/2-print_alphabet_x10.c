cc11fd91a1fb866357b9903be95c46140888d17f
sarkoful <dejireh@gmail.com>
2022-09-14 21:30:53 +0000

    my book

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,13 @@
 	*/
 void print_alphabet_x10(void)
 {
-	 char alpha;
-	  int index;
+	int count = 0;
+	char letter;
 
-	   alpha = 'a';
-
-	    for (index = 0; index < 10; index++)
-		     {
-			       while (alpha <= 'z')
-				         {
-						    _putchar((int) alpha);
-						       alpha++;
-						         }
-
-			        }
-	     _putchar((char) 10);
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
