4e7e587b686647ce84c0be04cde6bde6ca0bbed7
sarkoful <dejireh@gmail.com>
2022-09-14 20:53:04 +0000

    my phone

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,14 @@
 /**
  *  * print_alphabet - Prints all the alphabets, a-z
  *   *
- *    * Return: always 0 success
+ *    * Return: always 0 (success)
 	*/
 void print_alphabet(void)
 {
-	for (letter= ‘a’; letter<= ‘z’; letter++)
+	char letter;
 
-	putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	                  putchar (‘\n’) ;
-
-			                    getletter();
-
+	_putchar('\n');
 }
