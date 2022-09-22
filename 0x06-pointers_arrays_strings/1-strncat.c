f5cd68b82aecbd4b51777045a2f2a7f7d1f45d77
sarkoful <dejireh90@gmail.com>
2022-09-22 07:11:14 +0000

    good bye

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  * _strncat - function to append string
+ *   * @dest: destination string
+ *    * @src: Source string
+ *     * @n: number of bytes from src to be copied
+ *      * Return: Dest
+	*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+
+}
