7c6a3c693119e645a71c486f97d2bf9db33d55e9
sarkoful <dejireh@gmail.com>
2022-09-22 05:01:29 +0000

    Man man

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ *  * void prototypes()
+ *   * int prototypes()
+ *    * char prototypes()
+ *    */
+
+char *_strcat(char *dest, char *src);
+char *_str:ncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif/*MAIN_H*/
