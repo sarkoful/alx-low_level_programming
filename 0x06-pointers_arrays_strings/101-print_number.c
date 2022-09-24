fc2703007e0e7b6948e13cfdf3d10fb1c24ce214
sarkoful <dejireh90@gmail.com>
2022-09-24 04:47:56 +0000

    show me

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,43 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    * Return: On success 1.
- *     * On error, -1 is returned, and errno is set appropriately.
+ *  * print_number - Prints an integer.
+ *   * @n: The integer to be printed.
 	*/
-int _putchar(char c)
+
+void print_number(int n)
 {
-	return (write(1, &c, 1));
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}#include "main.h"
+
+/**
+ *  * print_number - Prints an integer.
+ *   * @n: The integer to be printed.
+ *    */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
