9fc96626bcee85fa45baffee1d6918ed4105bd37
sarkoful <dejireh90@gmail.com>
2022-09-27 08:21:40 +0000

    good coder

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
 /**
- * _string - gets length of a prefix substring
- * @s: string to check
- * @accept: string to check against
- *
- * Return: number of bytes of s in accept
- */
+ *  * _strspn - function to match strings from accept with part of s
+ *   * the strspn function gives you the number of consecutive characters in
+ *    * the first string, starting from the beginning, that are also in the
+ *     * second string.
+ *      * @s: first string
+ *       * @accept: second string
+ *        * Return: an integer number that counts from 1 to where accept stops
+	*/
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i;
+	int j;
+	unsigned int counter = 0;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
+				counter++;
 				break;
+			}
 		}
-		if (!accept[j])
-			break;
+		if (s[j] == '\0')
+			return (counter);
 	}
-
-	return (i);
+	return (counter);
 }
