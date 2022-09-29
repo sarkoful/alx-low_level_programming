7c2e4f844e98de99ade4c1884852a9ed77e7ff9b
sarkoful <dejireh90@gmail.com>
2022-09-29 01:18:31 +0000

    lets go

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,37 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to be used
- *
- * Return: the square root of n
- */
-int _sqrt_recursion(int n)
+ *  * power - function to get i*i (in determining square root)x
+ *   * @i: number to multiply
+ *    * @n: number we want to get square root of
+ *     * Return: i*i if number is a square root of n, or (i+1)*(i+1) if not
+ *      * stops iterating at n tho
+	*/
+int power(int i, int n)
 {
-	if (n == 1 || n == 0)
-		return (n);
-	return (_sqrt(0, n));
+	if ((i * i) == n)
+		return (i); /* the number is the square root of n*/
+	else if ((i * i) != n && ((i * i) < n))
+		return (power((i + 1), n));
+	else
+		return (-1);
 }
 
 /**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-int _sqrt(int n, int x)
+ *  * _sqrt_recursion - function to get natural square root recursively
+ *   * natural squareroot should  x such that x*x = n
+ *    * @n: number to get the square root of
+ *     * Return: sqrt of n or -1 if n does not have a natural square root
+ *      * This took an awful lot of time, but I feel a sense of pride getting this one
+ *       * even with the cryptic online assistance I got - Bravo
+	*/
+
+int _sqrt_recursion(int n)
 {
-	if (n > x / 2)
-	return (-1);
-	else if (n * n == x)
-	return (n);
-	return (_sqrt(n + 1, x));
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (power(1, n));
 }
-	
