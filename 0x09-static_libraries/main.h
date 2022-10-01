c1ac1472a9fb640d9935a98e72827248192eff37
sarkoful <dejireh90@gmail.com>
2022-10-01 03:17:51 +0000

    man

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* using MAIN_H in place of main.h defining it as a 'macro'*/
 
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +33,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN_H*/
+#endif
