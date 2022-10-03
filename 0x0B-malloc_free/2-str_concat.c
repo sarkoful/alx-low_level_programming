4b3591e004c44b7e6d81b6f769b1b201c47ba1e7
sarkoful <dejireh90@gmail.com>
2022-10-03 19:00:02 +0000

    three

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,10 +9,10 @@
  * case of success
  */
 
-char *str_concate(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
-	char *concate_str;
-	int index, concate_index = 0, len = 0;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,16 +23,16 @@ char *str_concate(char *s1, char *s2)
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 
-	concate_str = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (concate_str == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		concate_str[concat_index++] = s1[index];
+		concat_str[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		concate_str[concate_index++] = s2[index];
+		concat_str[concat_index++] = s2[index];
 
-	return (concate_str);
+	return (concat_str);
 }
