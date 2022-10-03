3224e7384d1652d5a0e76665ac4b26eb15c7a185
sarkoful <dejireh90@gmail.com>
2022-10-03 18:33:32 +0000

    two

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 
 /**
  * _strdup - returns a pointer to a newly allocated
- * space in memory, which contains a copy of the 
+ * space in memory, which contains a copy of the
  * string given as a parameter.
  * @str: string to be copied
  *
