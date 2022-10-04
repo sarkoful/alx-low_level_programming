1491b80d763dee915c814acecda3a54f3280facc
sarkoful <dejireh90@gmail.com>
2022-10-04 03:55:02 +0000

    man

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,78 +1,25 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * * wordCounterRec - count num of words recursively
- * * @str: pointer to char
- * * @i: current index
- * * Return: number of words
- */
-
-int wordCounterRec(char *str, int i)
+ *  * free_grid  - frees up a grid
+ *   * @grid: grid to check
+ *    * @height: height of grid
+	*/
+void free_grid(int **grid, int height)
 {
-	if (str[i] == '\0')
-		return (0);
-	if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
-		return (1 + wordCounterRec(str, i + 1));
-	return (wordCounterRec(str, i + 1));
-}
+	int i;
 
-/**
- * * word_counter - counts number of words in 1d array of strings
- * * @str: pointer to char
- * * Return: number of words
- */
-int word_counter(char *str)
-{
-	if (str[0] != ' ')
-		return (1 + wordCounterRec(str, 0));
-	return (wordCounterRec(str, 0));
-}
+	if (height <= 0)
+		return;
 
-/**
- * * strtow - splits a string into words.
- * * @str: string to be splitted
- * * Return: pointer to an array of strings (words) or null
- */
-char **strtow(char *str)
-{
-	char **strDup;
-	int i, n, m, words;
+	if (grid == NULL)
+		return;
 
-	if (str == NULL || str[0] == 0)
-		return (NULL);
-	words = word_counter(str);
-	if (words < 1)
-		return (NULL);
-	strDup = malloc(sizeof(char *) * (words + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = 0;
-	while (i < words && *str != '\0')
+	for (i = 0; i < height; i++)
 	{
-		if (*str != ' ')
-		{
-			n = 0;
-			while (str[n] != ' ')
-				n++;
-			strDup[i] = malloc(sizeof(char) * (n + 1));
-			if (strDup[i] == NULL)
-			{
-				while (--i >= 0)
-					free(strDup[--i]);
-				free(strDup);
-				return (NULL);
-			}
-			m = 0;
-			while (m < n)
-			{
-				strDup[i][m] = *str;
-				m++, str++;
-			}
-			strDup[i][m] = '\0';
-			i++;
-		}
-		str++;
+		free(grid[i]);
 	}
-	strDup[i] = '\0';
-	return (strDup);
+
+	free(grid);
 }
