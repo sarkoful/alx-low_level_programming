d7cd8a8d913fe8e8f695c124eb74276e79dbc41a
sarkoful <dejireh@gmail.com>
2022-10-05 01:34:35 -0800

    good

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,64 +1,33 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#define UNUSED(x) (void)(x)
 
 /**
-  * _isdigit - function to check if the string is a digit
-  *  * using the standard isdigit function in string.h
-  *   * @s: string to check
-  *    * Return: 1 if digit, 0 if not
-	*/
-
-int _isdigit(char *s)
-{
-	int k; /* k is a counter, and l is the return value*/
-
-	for (k = 0; s[k] != '\0'; k++)
-	{
-		if (isdigit(s[k]))
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
-  * main - function to print number of arguments passed
-  *  * @argc: number of arguments passed
-  *   * @argv: vector of arguments passed
-  *    * Return: 0
-	*/
-
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
 int main(int argc, char *argv[])
 {
-	int i, j = 0; /* i is a counter and j is to house the sum*/
+	int num, digit, sum = 0;
 
-
-	if (argc > 1)
+	for (num = 1; num < argc; num++)
 	{
-		for (i = 0; i < argc; i++)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			if (_isdigit(argv[i])) /* if it is a digit*/
-			{
-				j += atoi(argv[i]);
-			}
-			else /* if it is not a digit*/
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
 
-		printf("%d\n", j);
-		return (0);
+		sum += atoi(argv[num]);
 	}
 
-	else /* i.e. argc < 1*/
-	{
-		printf("%d\n", 0);
-		return (0); /* this return value was not stated in the question*/
-	}
+	printf("%d\n", sum);
 
+	return (0);
 }
