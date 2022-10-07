e1a4af62387b0713ba17dee787f30289f36e9a27
sarkoful <dejireh90@gmail.com>
2022-10-07 16:56:53 +0000

    goo man

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,21 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* using MAIN_H in place of main.h defining it as a 'macro'*/
 
-void print_array(int *a, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *s);
-char *cap_string(char *s);
-char *leet(char *s);
-char *rot13(char *s);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-
-#endif
+#endif /* MAIN_H */
