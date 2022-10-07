35b0c3aadebdecd8d236df4f7f098d19e66f9c12
sarkoful <dejireh90@gmail.com>
2022-10-07 17:02:01 +0000

    Happy man

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,27 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- *  * malloc_checked - function to check location to pointer
- *   * @b: parameter to the function
- *    * Return: Shouldn't return anything :( but returns t, which is a void format
+ *  * malloc_checked - allocate memory for b
+ *   * @b: whatever they give you
+ *    * Return: void pointer
 	*/
-=======
- * malloc_checked - function to check location to pointer
- * @b: parameter to the function
- * Return: Shouldn't return anything :( but returns t, which is a void format
- */
->>>>>>> e5a6e7a0a4d1105919466172cf51e8150e57c498
-
 void *malloc_checked(unsigned int b)
 {
-	void *t;
+	void *pointer = malloc(b);
 
-	t = malloc(b);
-	if (t == NULL)
+	if (pointer == NULL)
+	{
 		exit(98);
-
-	return (t);
+	}
+	return (pointer);
 }
