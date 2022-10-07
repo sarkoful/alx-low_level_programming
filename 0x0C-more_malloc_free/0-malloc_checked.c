0c079d1cd505790f2e3f8f76023bfc4659bf70e5
sarkoful <dejireh90@gmail.com>
2022-10-07 14:50:24 +0000

    Good

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  * malloc_checked - function to check location to pointer
+ *   * @b: parameter to the function
+ *    * Return: Shouldn't return anything :( but returns t, which is a void format
+	*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (t);
+}
